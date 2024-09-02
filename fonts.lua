8e76c057afd035885aca6561b8668571704fa795
Kedr2806 <87565042+Kedr2806@users.noreply.github.com>
2024-09-02 14:51:09 +0900

    Added fonts.lua

---
+++ fonts.lua
@@ -0,0 +1,16 @@
+local fonts = {}
+
+fonts = {
+	main = love.graphics.newFont("assets/fonts/Axmolotl.ttf", 16),
+	digitalDisco = love.graphics.newFont("assets/fonts/DigitalDisco-Thin.ttf", 16),
+	jfdot = love.graphics.newFont('assets/fonts/JF-Dot-K14-2004.ttf',14),
+	korean=love.graphics.newFont('assets/fonts/BMDOHYEON_ttf.ttf',16)
+}
+
+fonts.main:setLineHeight(0.75)
+fonts.digitalDisco:setLineHeight(0.75)
+
+
+fonts.digitalDisco:setFallbacks(fonts.jfdot)
+
+return fonts
