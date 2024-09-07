ce88ebd0ceb04b9065c517b8e82812909df56872
Kedr2806 <87565042+Kedr2806@users.noreply.github.com>
2024-09-07 16:44:50 +0900

    Update fonts.lua

--- fonts.lua
+++ fonts.lua
@@ -4,7 +4,7 @@ fonts = {
 	main = love.graphics.newFont("assets/fonts/Axmolotl.ttf", 16),
 	digitalDisco = love.graphics.newFont("assets/fonts/DigitalDisco-Thin.ttf", 16),
 	jfdot = love.graphics.newFont('assets/fonts/JF-Dot-K14-2004.ttf',14),
-	korean=love.graphics.newFont('assets/fonts/PFStardust_3.0_S.ttf',17,'mono',17)
+	korean=love.graphics.newFont('assets/fonts/PFStardust_3.0.ttf',15,'mono',15)
 }
 
 fonts.main:setLineHeight(1.3)
